945edc97edab78f1c2bc7c649d392232b5cfe3a9
riquemath <fredabisai@gmail.com>
2020-02-07 09:01:04 +0300

    Add Customer actions

--- src/app/customers/state/customer.actions.ts
+++ src/app/customers/state/customer.actions.ts
@@ -4,10 +4,22 @@ import { Customer } from '../customer.model';
 
 export enum CustomerActionTypes {
     LOAD_CUSTOMERS = '[Customer] Load Customers',
-    LOAD_CUSTOMERS_SUCCESS = '[Customer] Load Customer Success',
-    LOAD_CUSTOMERS_FAIL = '[Customer] Load Customer Fail'
+    LOAD_CUSTOMERS_SUCCESS = '[Customer] Load Customers Success',
+    LOAD_CUSTOMERS_FAIL = '[Customer] Load Customers Fail',
+    LOAD_CUSTOMER = '[Customer] Load Customer',
+    LOAD_CUSTOMER_SUCCESS = '[Customer] Load Customer Success',
+    LOAD_CUSTOMER_FAIL = '[Customer] Load Customer Fail',
+    CREATE_CUSTOMER = '[Customer] Create Customer',
+    CREATE_CUSTOMER_SUCCESS = '[Customer] Create Customer Success',
+    CREATE_CUSTOMER_FAIL = '[Customer] Create Customer Fail',
+    UPDATE_CUSTOMER = '[Customer] Update Customer',
+    UPDATE_CUSTOMER_SUCCESS = '[Customer] Update Customer Success',
+    UPDATE_CUSTOMER_FAIL = '[Customer] Update Customer Fail',
+    DELETE_CUSTOMER = '[Customer] Delete Customer',
+    DELETE_CUSTOMER_SUCCESS = '[Customer] Delete Customer Success',
+    DELETE_CUSTOMER_FAIL = '[Customer] Delete Customer Fail'
 }
-
+// Load Customers
 export class LoadCustomers implements Action {
     readonly type = CustomerActionTypes.LOAD_CUSTOMERS
 }
@@ -19,4 +31,65 @@ export class LoadCustomersFail implements Action {
     readonly type = CustomerActionTypes.LOAD_CUSTOMERS_FAIL
     constructor(public payload: string) {}
 }
-export type ActionType = LoadCustomers| LoadCustomersSuccess | LoadCustomersFail;
+
+// Load Customer
+export class LoadCustomer implements Action {
+    readonly type = CustomerActionTypes.LOAD_CUSTOMER
+    constructor(public payload: number) {}
+}
+export class LoadCustomerSuccess implements Action {
+    readonly type = CustomerActionTypes.LOAD_CUSTOMER_SUCCESS
+    constructor(public payload: Customer) {}
+}
+export class LoadCustomerFail implements Action {
+    readonly type = CustomerActionTypes.LOAD_CUSTOMER_FAIL
+    constructor(public payload: string) {}
+}
+
+// Create Customer
+export class CreateCustomer implements Action {
+    readonly type = CustomerActionTypes.CREATE_CUSTOMER
+    constructor(public payload: Customer) {}
+}
+export class CreateCustomerSuccess implements Action {
+    readonly type = CustomerActionTypes.CREATE_CUSTOMER_SUCCESS
+    constructor(public payload: Customer) {}
+}
+export class CreateCustomerFail implements Action {
+    readonly type = CustomerActionTypes.CREATE_CUSTOMER_FAIL
+    constructor(public payload: string) {}
+}
+
+// Update Customer
+export class UpdateCustomer implements Action {
+    readonly type = CustomerActionTypes.UPDATE_CUSTOMER
+    constructor(public payload: Customer) {}
+}
+export class UpdateCustomerSuccess implements Action {
+    readonly type = CustomerActionTypes.UPDATE_CUSTOMER_SUCCESS
+    constructor(public payload: Customer) {}
+}
+export class UpdateCustomerFail implements Action {
+    readonly type = CustomerActionTypes.UPDATE_CUSTOMER_FAIL
+    constructor(public payload: string) {}
+}
+
+// Delete Customer
+export class DeleteCustomer implements Action {
+    readonly type = CustomerActionTypes.DELETE_CUSTOMER
+    constructor(public payload: number) {}
+}
+export class DeleteCustomerSuccess implements Action {
+    readonly type = CustomerActionTypes.DELETE_CUSTOMER_SUCCESS
+    constructor(public payload: number) {}
+}
+export class DeleteCustomerFail implements Action {
+    readonly type = CustomerActionTypes.DELETE_CUSTOMER_FAIL
+    constructor(public payload: string) {}
+}
+
+export type ActionType = LoadCustomers| LoadCustomersSuccess | LoadCustomersFail |
+                         LoadCustomer | LoadCustomerSuccess | LoadCustomerFail |
+                         CreateCustomer | CreateCustomerSuccess | CreateCustomerFail | 
+                         DeleteCustomer | DeleteCustomerSuccess | DeleteCustomerFail
+                         ;
